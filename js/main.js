8eee31c5eaf348a0b2c14f67282bd83d2adf55ef
Alexandre Clament <laexou.24@gmail.com>
2022-07-28 19:58:23 +0200

    implémentation slider partie 1
    implémentation slider partie 1

--- js/main.js
+++ js/main.js
@@ -7,6 +7,87 @@ const popupProduct = document.querySelector('.cart-pop-up')
 const cartView = document.querySelector('.header-cart-svg')
 // cart content
 const cartContent = document.querySelector('.cart-pop-up-content-product')
+// images slider
+const imagesSlider = document.querySelectorAll('img.other-img-content')
+const imagesSliderMain = document.querySelector('.img-main')
+// slider previous / next 
+const previous = document.querySelector('.previous-slide ')
+const next = document.querySelector('.next-slide ')
+// get length image tab
+const numberOfSlide = imagesSlider.length
+
+
+
+
+
+next.addEventListener('click', () => { 
+
+    // trouver l'élément qui à la classe .selected
+    let tableau = [] 
+
+    let imageSelected = null
+
+
+    imagesSlider.forEach((imageSlider, index ) => {
+        tableau[index] = imageSlider.classList.value
+        
+        // if (tableau[index].indexOf('img-selected') >= 0 ) {
+        //     imageSelected = index
+        // }
+        
+    })
+
+    imageSelected = tableau.findIndex((elementDuTableau) => 
+
+        elementDuTableau.indexOf('img-selected') >= 0
+        
+    )
+
+    console.log(tableau)
+    console.log(imageSelected)
+
+    // imageSelected est l'index qui possède la class selected-img
+
+    // au click de next afficher l'image à l'index suivant
+
+    // au click de next afficher l'image de la miniature suivante
+
+    // TODO test sans chaine de caractères dans le tableau tableau[]
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 openMenu.addEventListener("click", () => {
